d974c60efac360c2b2206a8a098ecdc0dffc41aa
TamingSari <gohsyang@gmail.com>
2023-11-28 13:07:30 +0800

    refactor: update next config

--- next.config.js
+++ next.config.js
@@ -1,29 +1,31 @@
 /** @type {import('next').NextConfig} */
-export const reactStrictMode = true;
-export const images = {
-  domains: [
-    "www.google.com",
-    "avatar.vercel.sh",
-    "faisalman.github.io",
-    "avatars.dicebear.com",
-    "res.cloudinary.com",
-    "pbs.twimg.com",
-  ],
+module.exports = {
+  reactStrictMode: true,
+  images: {
+    domains: [
+      "www.google.com",
+      "avatar.vercel.sh",
+      "faisalman.github.io",
+      "avatars.dicebear.com",
+      "res.cloudinary.com",
+      "pbs.twimg.com",
+    ],
+  },
+  async headers() {
+    return [
+      {
+        source: "/:path*",
+        headers: [
+          {
+            key: "Referrer-Policy",
+            value: "no-referrer-when-downgrade",
+          },
+          {
+            key: "X-DNS-Prefetch-Control",
+            value: "on",
+          },
+        ],
+      },
+    ];
+  },
 };
-export async function headers() {
-  return [
-    {
-      source: "/:path*",
-      headers: [
-        {
-          key: "Referrer-Policy",
-          value: "no-referrer-when-downgrade",
-        },
-        {
-          key: "X-DNS-Prefetch-Control",
-          value: "on",
-        },
-      ],
-    },
-  ];
-}
